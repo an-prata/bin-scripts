2e9aea385e5b2e86f26692cdb9daa3b3254d5838
Evan Overman <evanrileyoverman@gmail.com>
2023-06-23 11:53:03 -0700

    delete for move to bspwm config

--- layout-switch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ `ibus engine | awk -F":" '{ print $2 }'` == "us" ]]; then
-	ibus engine 'anthy'
-	setxkbmap -layout jp
-else
-	ibus engine 'xkb:us::eng'
-	setxkbmap -layout us
-fi
